e9935d7b3356427904b51e4c50a11f014c764db6
Matthijs Otterloo <matthijs@cloudmoosic.com>
2015-07-03 15:09:13 +0200

    .

--- Mamorgen/Mamorgen.iOS/ViewController.designer.cs
+++ Mamorgen/Mamorgen.iOS/ViewController.designer.cs
@@ -1,15 +1,18 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace Mamorgen.iOS
 {
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		
 		void ReleaseDesignerOutlets ()
 		{
 		}
